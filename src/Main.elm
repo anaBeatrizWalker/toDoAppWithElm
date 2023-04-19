7bd64d7ea3af162e8562867d89a73e16606510c0
anaBeatrizWalker <anabiawalker@gmail.com>
2023-04-19 13:21:24 -0300

    Renderizando as tarefas

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,10 @@
 module Main exposing (main)
 
 import Browser
-import Html.Styled exposing (Html, div, form, input, button, text)
-import Html.Styled.Attributes exposing (type_, value)
+import Html.Styled exposing (Html, div, form, input, button, text, ul, li)
+import Html.Styled.Attributes exposing (type_, value, css)
 import Html.Styled.Events exposing (onInput, onSubmit)
+import Css
 
 type Msg = UpdateNewToDo String | SubmitToDo
 type alias Model = {newToDo : String, toDos: List ToDo}
@@ -24,9 +25,11 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
+    div [ css [Css.maxWidth (Css.px 280)] ] --tamanho 280px
         [ Html.Styled.form [onSubmit SubmitToDo] [
             input [type_ "text", value model.newToDo, onInput UpdateNewToDo] []
             , button [type_ "submit"] [text "Submit"]
         ]
+        , ul []
+            (List.map (\ toDo -> li [] [text toDo.task]) model.toDos)
         ]
